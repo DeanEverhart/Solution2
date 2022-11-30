b10422fc9836c1df4d20145f6b3eada4c1793b1f
Dean Everhart <39904889+DeanEverhart@users.noreply.github.com>
2022-11-30 12:36:04 -0500

    Add files via upload

---
+++ Model.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models
+{
+    public class Model
+    {
+        public int ID { get; set; }
+
+        public string? Text { get; set; }
+
+        public int? Number { get; set; }
+
+        public bool? Bool { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? Date { get; set; }
+    }
+}
